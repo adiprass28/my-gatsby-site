97578a00d4a9d9de3f30e01ea883e6fb62577dc5
Adi Prastiyo <adiprass28@gmail.com>
2021-10-05 13:44:39 +0800

    Menyelesaikan Gatsby Tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+const about = () => {
+    return (
+        <Layout pageTitle='Tentang Saya'>
+            <p>Hi, saya membangun situs ini dengan Gatsby</p>
+        </Layout>
+    );
+}
+
+export default about;
